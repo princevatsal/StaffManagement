27f74914ad51fc7696b0d989da62a17286a485ff
ayushk01 <ayushkashyap@outlook.com>
2020-04-13 02:41:12 +0530

    Added Firebase Config

--- src/Fire.js
+++ src/Fire.js
@@ -1,15 +1,6 @@
 import firebase from 'firebase';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyDiNSHCgtwdxbZd_131nsx7CCo7Uaz-TBw',
-  authDomain: 'staff-management-753e4.firebaseapp.com',
-  databaseURL: 'https://staff-management-753e4.firebaseio.com',
-  projectId: 'staff-management-753e4',
-  storageBucket: 'staff-management-753e4.appspot.com',
-  messagingSenderId: '852955534775',
-  appId: '1:852955534775:web:ed8426864083077d158432',
-  measurementId: 'G-5RSWJ92NH3',
-};
+import firebaseConfig from '../config';
 
 firebase.initializeApp(firebaseConfig);
 console.log('Firebase Initialized');
