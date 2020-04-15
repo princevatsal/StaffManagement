e019e7d1100135431a4f4cb0eba1c521c165e31b
Priyansh Vatsal <priyanshvatsal@gmail.com>
2020-04-15 16:16:55 +0530

    profile page ready

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -1,7 +1,9 @@
 // local imgs
 
 const RegisterBackground = require('../assets/imgs/register-bg.png');
-
+const ProfileBackground = require('../assets/imgs/bg5.png');
 export default {
+  RegisterBackground,
+  ProfileBackground,
   RegisterBackground,
 };
